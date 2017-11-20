91e241e9553b146e59cbce3d7749ac6d093c3e4e
itikhonov <riyuzaky@gmail.com>
2017-11-20 17:18:51 +0300

    Initial Commit

---
+++ Passing Data Between ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Passing Data Between ViewControllers
+//
+//  Created by Ivan Tikhonov on 20/11/2017.
+//  Copyright © 2017 Ivan Tikhonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
